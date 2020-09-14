0cbe8aecb639845b1716142038fca201d5cf22ca
TylerLeung <leung.tyler008@gmail.com>
2020-09-14 04:12:37 -0400

    Updates

---
+++ precache-manifest.210f39f74c2005d8cc8d24efc812d998.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed5ab297906b0e4a0e68827a3ddd4072",
+    "url": "/index.html"
+  },
+  {
+    "revision": "54d882e3074a1a80e6ec",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "2489872a692b31df10f3",
+    "url": "/static/js/2.32b4e0f9.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.32b4e0f9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "54d882e3074a1a80e6ec",
+    "url": "/static/js/main.a7a0af47.chunk.js"
+  },
+  {
+    "revision": "f139b624154a37532bb8",
+    "url": "/static/js/runtime-main.ce89729d.js"
+  }
+]);
